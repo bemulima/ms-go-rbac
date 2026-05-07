99cffad169bacba6afa38b7cf3e65f7468e2f4e1
Black Panther <bemulima@gmail.com>
2026-05-07 21:30:03 +0500

    Rename ms-getway references to ms-gateway

--- test/e2e/gateway/scenarios/01_api_and_admin_gateway.sh
+++ test/e2e/gateway/scenarios/01_api_and_admin_gateway.sh
@@ -11,7 +11,7 @@ raw="$(http_json "${GATEWAY_URL}" "PATCH" "${RBAC_API}/principal-role/update" "$
 st="$(printf '%s\n' "${raw}" | extract_status)"
 resp="$(printf '%s\n' "${raw}" | extract_body)"
 if [[ "${st}" != "200" ]]; then
-  record_mismatch "${wiki_ref} (principal-role/update)" "HTTP 200" "HTTP ${st}" "PATCH ${RBAC_API}/principal-role/update resp=${resp}" "major" "ms-go-rbac/ms-getway"
+  record_mismatch "${wiki_ref} (principal-role/update)" "HTTP 200" "HTTP ${st}" "PATCH ${RBAC_API}/principal-role/update resp=${resp}" "major" "ms-go-rbac/ms-gateway"
   return 0
 fi
 record_ok "rbac principal-role/update returns 200"
@@ -20,7 +20,7 @@ raw="$(http_get "${GATEWAY_URL}" "${RBAC_API}/principal-role/get?user_id=${user_
 st="$(printf '%s\n' "${raw}" | extract_status)"
 resp="$(printf '%s\n' "${raw}" | extract_body)"
 if [[ "${st}" != "200" ]]; then
-  record_mismatch "${wiki_ref} (principal-role/get)" "HTTP 200" "HTTP ${st}" "GET ${RBAC_API}/principal-role/get resp=${resp}" "major" "ms-go-rbac/ms-getway"
+  record_mismatch "${wiki_ref} (principal-role/get)" "HTTP 200" "HTTP ${st}" "GET ${RBAC_API}/principal-role/get resp=${resp}" "major" "ms-go-rbac/ms-gateway"
   return 0
 fi
 record_ok "rbac principal-role/get returns 200"
@@ -30,7 +30,7 @@ admin_raw="$(http_get "${ADMIN_GATEWAY_URL}" "${RBAC_API}/service-list?page=1&pa
 admin_st="$(printf '%s\n' "${admin_raw}" | extract_status)"
 admin_resp="$(printf '%s\n' "${admin_raw}" | extract_body)"
 if [[ "${admin_st}" != "200" ]]; then
-  record_mismatch "${wiki_ref} (admin gateway)" "HTTP 200 admin service-list" "HTTP ${admin_st} (admin gateway)" "GET ${ADMIN_GATEWAY_URL}${RBAC_API}/service-list resp=${admin_resp}" "minor" "ms-getway/ms-go-rbac"
+  record_mismatch "${wiki_ref} (admin gateway)" "HTTP 200 admin service-list" "HTTP ${admin_st} (admin gateway)" "GET ${ADMIN_GATEWAY_URL}${RBAC_API}/service-list resp=${admin_resp}" "minor" "ms-gateway/ms-go-rbac"
 else
   record_ok "rbac admin service-list reachable via admin gateway"
 fi
